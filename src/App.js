306617991e2f0bb65d7e621fa256871b3911e727
Jarred Wilson <jarred.wilson12@gmail.com>
2020-08-23 15:06:44 -0400

    Add beginning pokemon example

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import StatefulCC from './sample/StatefulCC';
 import StatelessCC from './sample/StatelessCC';
 import StatefulFC from './sample/StatefulFC';
 import StatelessFC from './sample/StatelessFC';
+// import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <StatefulCC/>
-      <StatelessCC
-        title='StatelessCC'
-      />
-      <StatefulFC/>
-      <StatelessFC
-        title='StatelessFC'
-      />
+class App extends Component {
 
-    </div>
-  );
+  // state = {
+  //   data: null
+  // }
+
+  // getData = async () => await axios.get('https://pokeapi.co/api/v2/pokemon?limit=3');
+
+  // componentDidMount() {
+  //   if(this.state.data === null) 
+  //     this.getData()
+  //     .then(data => this.setState({data}));
+  // }
+
+  render() {
+    return (
+      <div className="App">
+        <StatefulCC/>
+        <StatelessCC
+          title='StatelessCC'
+        />
+        <StatefulFC/>
+        <StatelessFC
+          title='StatelessFC'
+        />
+
+        {/* {this.state.data !== null && this.state.data.data.results
+        .map(item => 
+          item.name
+        )} */}
+
+      </div>
+    );
+  }
 }
 
 export default App;
